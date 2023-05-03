1838fc51b5b48b0b819b84544b8bc09378f8b80e
Alexey Kadantsev <alexey.kadantsev@kodep.ru>
2023-05-03 13:52:32 +0300

    fix migration

--- src/db/migrations/1683110974719-Init.ts
+++ src/db/migrations/1683110974719-Init.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1683035324062 implements MigrationInterface {
-    name = 'Init1683035324062'
+export class Init1683110974719 implements MigrationInterface {
+    name = 'Init1683110974719'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "favorite_movies" ("userId" uuid NOT NULL, "movie" numeric NOT NULL, "watched" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_ad08da426f2a5c6c7ba76a193ad" PRIMARY KEY ("userId"))`);
+        await queryRunner.query(`CREATE TABLE "favorite_movies" ("userId" uuid NOT NULL, "movieId" numeric NOT NULL, "watched" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_ad08da426f2a5c6c7ba76a193ad" PRIMARY KEY ("userId"))`);
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying(255) NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "favorite_genres" ("userId" uuid NOT NULL, "genreId" numeric NOT NULL, CONSTRAINT "PK_6c6c06c2ea12ff68b8d2c58aacd" PRIMARY KEY ("userId"))`);
         await queryRunner.query(`ALTER TABLE "favorite_movies" ADD CONSTRAINT "FK_ad08da426f2a5c6c7ba76a193ad" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
